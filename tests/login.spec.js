1929b582d10cfdb902478d0c8e8ca0179cc31fb5
Denis Lopes <denislopes80@gmail.com>
2025-05-23 18:27:42 -0300

    Implementando PageObject tela Login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,51 +1,45 @@
 import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
+import { LoginPage } from '../pages/loginPage';
+import { DashPage } from '../pages/DashPage';
 
 test('Verificação em duas etapas invalida', async ({ page }) => {
 
-  const usuario ={
+  const loginPage = new LoginPage(page);
+
+  const usuario = {
     cpf: '00000014141',
-    senhaVerificacao: '147258'
+    senha: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for(const digito of usuario.senhaVerificacao){
-      await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.acessarPagina();
+  await loginPage.informarCpf(usuario.cpf);
+  await loginPage.informarSenha(usuario.senha);
+  await loginPage.informar2FA('123456');
 
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
 });
 
 test('Realizar login com sucesso', async ({ page }) => {
 
-  const usuario ={
+  const loginPage = new LoginPage(page);
+  const dashPage = new DashPage(page);
+
+  const usuario = {
     cpf: '00000014141',
     senhaVerificacao: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessarPagina();
+  await loginPage.informarCpf(usuario.cpf);
+  await loginPage.informarSenha(usuario.senhaVerificacao);
 
-  for(const digito of usuario.senhaVerificacao){
-      await page.getByRole('button', { name: digito }).click();
-  }
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-   await page.waitForTimeout(3000)
+  await page.waitForTimeout(3000)
   const code = await obterCodigo2FA();
 
-  await page.getByRole('textbox', { name: '000000' }).fill(code);
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  loginPage.informar2FA(code)
 
   await page.waitForTimeout(2000)
 
-  await expect(page.locator('#account-balance')).toHaveText('R$ 5.000,00')
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
